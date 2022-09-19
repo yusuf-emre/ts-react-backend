435089c7fdbe8e6268509130667d3a096bbce06b
Yusuf Emre Velioglu <velioglu.ye@gmail.com>
2022-09-19 09:22:48 +0200

    add start server

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,25 @@ mongoose
     .connect(config.mongo.url)
     .then(() => {
         console.log('CONNECTED');
+        StartServer();
     })
     .catch(error => {
         console.log(error);
     });
+
+//Only start the server if Mongo connects
+const StartServer = () => {
+    //Healthcheck
+    app.get('/hej', (req, res, next) => res.status(200).json({ message: 'hej hej' }));
+    
+    //Error handling
+    app.use((req, res, next) => {
+        const error = new Error('NOT FOUND!');
+        console.log(error);
+
+        return res.status(404).json({ message: error.message})
+    })
+
+    http.createServer(app)
+        .listen(config.server.port, () => console.log(`Server is running on port ${config.server.port}.`));
+}
