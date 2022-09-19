b63721f474aa2bb2500a7dae3e7bd488749f75fd
Yusuf Emre Velioglu <velioglu.ye@gmail.com>
2022-09-19 10:10:59 +0200

    create controller

---
+++ src/controllers/Puppy.ts
@@ -0,0 +1,68 @@
+import { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
+import Puppy from "../models/Puppy";
+
+const createPuppy = (req: Request, res: Response, next: NextFunction) => {
+    const { breed, name, dob, size, img } = req.body;
+
+    const puppy = new Puppy({
+        id: new mongoose.Types.ObjectId(),
+        breed,
+        name,
+        dob,
+        size,
+        img
+    })
+
+    return puppy
+        .save()
+        .then(puppy => res.status(201).json({ puppy }))
+        .catch(error => res.status(500).json({ error }));
+}
+const readPuppy = (req: Request, res: Response, next: NextFunction) => {
+    const puppyId = req.params.puppyId;
+
+    return Puppy.findById(puppyId)
+        .then(puppy => (puppy ? res.status(200).json({ puppy }) : res.status(404).json({message: 'Not Found'})))
+        .catch(error => res.status(500).json({ error }));
+
+}
+const readAllPuppies = (req: Request, res: Response, next: NextFunction) => {
+    return Puppy.find()
+        .then(puppies => res.status(200).json({ puppies }))
+        .catch(error => res.status(500).json({ error }));
+}
+const updatePuppy = (req: Request, res: Response, next: NextFunction) => {
+    const puppyId = req.params.puppyId;
+
+    return Puppy.findById(puppyId)
+        .then(puppy => {
+            if(puppy) {
+                puppy.set(req.body)
+                
+                return puppy
+                    .save()
+                    .then(puppy => res.status(201).json({ puppy }))
+                    .catch(error => res.status(500).json({ error }));
+            }
+            else {
+                res.status(404).json({message: 'Not Found'});
+            }
+        })
+        .catch(error => res.status(500).json({ error }));
+}
+const deletePuppy = (req: Request, res: Response, next: NextFunction) => {
+    const puppyId = req.params.puppyId;
+
+    return Puppy.findByIdAndDelete(puppyId)
+        .then(puppy => (puppy ? res.status(201).json({ message: 'Deleted' }) : res.status(404).json({message: 'Not Found'})))
+        .catch(error => res.status(500).json({ error }));
+}
+
+export default {
+    createPuppy,
+    readPuppy,
+    readAllPuppies,
+    updatePuppy,
+    deletePuppy
+}
